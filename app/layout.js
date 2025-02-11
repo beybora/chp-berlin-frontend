dc747412ac6c8159ed790cb301275eb44d821102
beybora <bora.b.sarikaya@gmail.com>
2025-02-11 22:56:22 +0100

    feat: add footer to layout

--- app/layout.js
+++ app/layout.js
@@ -4,30 +4,34 @@ import { Poppins, Aboreto } from "next/font/google";
 import { getMenu } from "utils/getMenu";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Footer from "components/Footer/Footer";
 config.autoAddCss = false;
 
 const poppins = Poppins({
-    weight: ["400", "700"],
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-poppins",
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
 });
 
 const aboreto = Aboreto({
-    weight: ["400"],
-    subsets: ["latin"],
-    display: "swap",
-    variable: "--font-aboreto",
+  weight: ["400"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-aboreto",
 });
 
 export default async function RootLayout({ children }) {
-    const data = await getMenu();
-    return (
-        <html lang="en" className={`${poppins.variable} ${aboreto.variable}`}>
-            <body className="font-body">
-                <MainMenu menuItems={data} />
-                {children}
-            </body>
-        </html>
-    ) 
+  const data = await getMenu();
+  return (
+    <html lang="en" className={`${poppins.variable} ${aboreto.variable}`}>
+      <body className="font-body">
+        <MainMenu menuItems={data} />
+        {children}
+        <footer>
+          <Footer />
+        </footer>
+      </body>
+    </html>
+  );
 }
