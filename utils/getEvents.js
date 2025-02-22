64d9746cde16f28068ab4e67ee4559b57bde59a3
beybora <bora.b.sarikaya@gmail.com>
2025-02-22 10:27:29 +0100

    feat: create getEvents utils function

---
+++ utils/getEvents.js
@@ -0,0 +1,38 @@
+import { cleanAndTransformBlocks } from "./cleanAndTransformBlocks";
+
+export const getEvents = async () => {
+  const params = {
+    query: `
+   query EventsQuery {
+     events {
+       nodes {
+         acf {
+            date
+            description
+            time
+            name
+          }
+           id
+          title
+        }
+      }
+    }
+  `,
+  };
+  
+  const response = await fetch(process.env.WP_GRAPHQL_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  });
+
+  const { data } = await response.json();
+
+  if (!data?.events?.nodes) {
+    return null;
+  }
+
+  return data.events.nodes;
+};
