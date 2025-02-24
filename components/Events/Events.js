7165832d6bf87dd5361e31dee6b5fe0fcc0b5161
beybora <bora.b.sarikaya@gmail.com>
2025-02-24 20:12:18 +0100

    feat: add "alle veranstaltungen" button

--- components/Events/Events.js
+++ components/Events/Events.js
@@ -1,6 +1,7 @@
 import React from "react";
 import EventCarousel from "./EventCarousel/EventCarousel";
 import { getEvents } from "../../utils/getEvents";
+import ButtonLink from "components/ButtonLink/ButtonLink";
 
 const Events = async () => {
   const events = await getEvents();
@@ -9,6 +10,9 @@ const Events = async () => {
   return (
     <div className=" mx-auto px-4 py-8 bg-red-500">
       <EventCarousel events={events} />
+      <div className="flex justify-center">
+        <ButtonLink destination="/events" label="Alle Veranstaltungen anzeigen" type="white" />
+      </div>
     </div>
   );
 };
