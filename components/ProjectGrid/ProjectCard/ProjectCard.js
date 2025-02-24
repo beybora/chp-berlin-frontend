1bcfc3a40b0995df0af1d65e1371ee024876e471
beybora <bora.b.sarikaya@gmail.com>
2025-02-24 21:53:32 +0100

    chore: adjust height of project cards

--- components/ProjectGrid/ProjectCard/ProjectCard.js
+++ components/ProjectGrid/ProjectCard/ProjectCard.js
@@ -8,7 +8,7 @@ import ButtonLink from "components/ButtonLink/ButtonLink";
         <div className="absolute -bottom-2 -right-2 bg-black w-full h-full"></div>
   
         {/* Weiße Karte mit fixer Höhe */}
-        <div className="relative bg-white p-4 shadow-lg min-h-[220px] flex flex-col justify-between overflow-hidden">
+        <div className="relative bg-white p-4 shadow-lg min-h-[160px] flex flex-col justify-between overflow-hidden">
           {/* <img src={image} alt={title} className="w-full h-42 object-cover" /> */}
           <div className="mt-4 flex flex-col flex-grow">
             <h3 className="text-h5 font-bold text-black">{title}</h3>
