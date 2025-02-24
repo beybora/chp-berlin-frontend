936844008e208677184c87317f042fb0cf3ecafa
beybora <bora.b.sarikaya@gmail.com>
2025-02-24 22:17:49 +0100

    feat: create work and works page

---
+++ app/work/[...slug]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkPage = () => {
+  return (
+    <div>
+      <h1>Work Page</h1>
+    </div>
+  )
+}
+
+export default WorkPage;
---
+++ app/work/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllWorkPage = () => {
+  return (
+    <div>
+      <h1>All Work Page</h1>
+    </div>
+  )
+}
+
+export default AllWorkPage;
