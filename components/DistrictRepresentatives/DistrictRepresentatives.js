76b67139442a872c2c08199092aefbbb218a08f2
beybora <bora.b.sarikaya@gmail.com>
2025-03-04 18:08:00 +0100

    feat: create distrcit  reppresentatives component

---
+++ components/DistrictRepresentatives/DistrictRepresentatives.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const DistrictRepresentatives = () => {
+  return (
+    <div className=" mx-auto px-4 py-8 bg-red-500">
+      <div className="max-w-5xl mx-auto">
+        <h1 className="text-3xl font-bold flex justify-center">
+          District Representatives
+        </h1>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="bg-white p-4 rounded-2xl flex items-center flex-col ">
+            <h2 className="text-lg font-bold ">District Representative</h2>
+            <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+          </div>
+          <div className="bg-white p-4 rounded-2xl flex flex-col items-center ">
+            <h2 className="text-lg font-bold">District Representative</h2>
+            <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+          </div>
+          <div className="bg-white p-4 rounded-2xl flex flex-col items-center ">
+            <h2 className="text-lg font-bold">District Representative</h2>
+            <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+          </div>
+          <div className="bg-white p-4 rounded-2xl flex flex-col items-center ">
+            <h2 className="text-lg font-bold">District Representative</h2>
+            <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+              <h1>District Representatives</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DistrictRepresentatives;
