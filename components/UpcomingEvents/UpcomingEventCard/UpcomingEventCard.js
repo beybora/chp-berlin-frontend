5f51aef6673516661e9f850cf4c1b1c1fe12b0c7
beybora <bora.b.sarikaya@gmail.com>
2025-03-05 21:22:22 +0100

    refactor: refactor flex layout

--- components/UpcomingEvents/UpcomingEventCard/UpcomingEventCard.js
+++ components/UpcomingEvents/UpcomingEventCard/UpcomingEventCard.js
@@ -7,7 +7,7 @@ const UpcomingEventCard = ({ date, title, description, time }) => {
   const [day, month, year] = date ? date.split('/') : ['', '', ''];
 
   return (
-      <div className="flex flex-col md:flex-row items-left space-x-4 w-full md:w-1/2 p-2">
+      <div className="flex flex-row items-left space-x-4 w-full md:w-1/2 p-2">
           <div className="bg-[#035D5D] text-white rounded-md p-3 w-16 flex flex-col items-center justify-center">
               <span className="text-h7 font-heading font-bold">{day}</span>
               <span className="text-above-title uppercase">{getGermanMonth(month)}</span>
