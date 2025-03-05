42fa8632eaf081e6b927ea66c6710a9ba5c93ee3
beybora <bora.b.sarikaya@gmail.com>
2025-03-05 21:23:52 +0100

    feat: add getAdministrationMembers utils function to query administration members

---
+++ utils/getAdministrationMembers.js
@@ -0,0 +1,33 @@
+  export const getAdministrationMembers = async () => {
+    const params = {
+      query: `
+    query DistrictQuery {
+    allYonetimKurulu {
+      nodes {
+        acfFelder {
+          name
+          position
+        }
+        id
+      }
+    }
+  }
+      `,
+    };
+
+    const response = await fetch(process.env.WP_GRAPHQL_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    });
+
+    const { data } = await response.json();
+
+    if (!data?.allYonetimKurulu?.nodes) {
+      return null;
+    }
+    
+    return data.allYonetimKurulu.nodes;
+  };
