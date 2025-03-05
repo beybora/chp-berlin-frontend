a3f68a2127b4d563ef961013997eb675ab1b124f
beybora <bora.b.sarikaya@gmail.com>
2025-03-05 21:23:08 +0100

    feat: adjust District Representrative Component

--- components/DistrictRepresentatives/DistrictRepresentatives.js
+++ components/DistrictRepresentatives/DistrictRepresentatives.js
@@ -1,45 +1,36 @@
-import React from 'react'
+import React from "react";
+import { getAdministrationMembers } from "utils/getAdministrationMembers";
 
-const DistrictRepresentatives = () => {
+const DistrictRepresentatives = async ({ attributes }) => {
+  const administrationMembers = await getAdministrationMembers();
+
   return (
-    <div className=" mx-auto px-4 py-8 bg-red-500">
-      <div className="max-w-5xl mx-auto">
+    <div className="w-full py-10">
+      <div className="max-w-5xl mx-auto container">
         <h1 className="text-3xl font-bold flex justify-center">
-          District Representatives
+          CHP Berlin Birliği İlçe Temsilcileri
         </h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          <div className="bg-white p-4 rounded-2xl flex items-center flex-col ">
-            <h2 className="text-lg font-bold ">District Representative</h2>
-            <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-          </div>
-          <div className="bg-white p-4 rounded-2xl flex flex-col items-center ">
-            <h2 className="text-lg font-bold">District Representative</h2>
-            <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-          </div>
-          <div className="bg-white p-4 rounded-2xl flex flex-col items-center ">
-            <h2 className="text-lg font-bold">District Representative</h2>
-            <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-          </div>
-          <div className="bg-white p-4 rounded-2xl flex flex-col items-center ">
-            <h2 className="text-lg font-bold">District Representative</h2>
-            <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-              <h1>District Representatives</h1>
-          </div>
+        <p className="text-center text-gray-500 py-5">
+          CHP Berlin Birliği'nin İlçe Temsilcileri, Berlin'in farklı
+          bölgelerinde partiyi temsil eden ve yerel düzeyde çalışmalar yürüten
+          önemli aktörlerdir. Bu temsilciler, bölgesel organizasyonları
+          düzenleyerek CHP üyelerini bir araya getirir, toplumsal konulara
+          duyarlılık oluşturur ve yerel sorunlara çözüm üretmek için aktif rol
+          oynarlar.
+        </p>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-20 py-10">
+          {administrationMembers.map((member) => (
+            <div key={member.id}>
+              <h2 className="text-lg font-bold">{member.acfFelder.name}</h2>
+              <p className="text-sm text-gray-500">
+                {member.acfFelder.position}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default DistrictRepresentatives;
