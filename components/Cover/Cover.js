768974161f937c1be6e05f17c37a5727b32b4c8c
beybora <bora.b.sarikaya@gmail.com>
2025-03-06 22:31:53 +0100

    style: minor layout changes

--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -3,7 +3,10 @@ import React from "react";
 
 const Cover = ({ title, subtitle, background }) => {
   return (
-    <div className="h-[60vh] bg-cover relative min-h-[400px] flex items-center justify-start w-full px-10" src={background}>
+    <div
+      className="h-[60vh] bg-cover relative min-h-[400px] flex items-center justify-start w-full px-10"
+      src={background}
+    >
       <Image
         src={background}
         alt="cover"
@@ -14,12 +17,18 @@ const Cover = ({ title, subtitle, background }) => {
       />
       {/* Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-70"></div>
-      <div className="max-w-5xl z-10 text-white text-left">
-        <div>
-          <h1 className="text-5xl font-bold leading-tight border-b-4 border-blue-500 inline-block pb-1">
-            {title}
+      <div className="container z-10 text-white text-left">
+        <div clas>
+          <h1 className="text-5xl font-bold leading-tight inline-block pb-1">
+            CHP Berlin Birliği
           </h1>
-          <p className="text-lg mt-4">test</p>
+          <p className="text-lg mt-4">
+            CHP Berlin Birliği, Almanya’da Cumhuriyet Halk Partisi’nin
+            temsilcisi olarak demokrasi, özgürlük ve adalet değerlerini savunur.
+            Berlin’de yaşayan yurttaşları bir araya getirerek sosyal dayanışmayı
+            güçlendirmeyi, Türkiye ve Almanya arasındaki ilişkileri geliştirmeyi
+            hedefler.
+          </p>
         </div>
       </div>
     </div>
