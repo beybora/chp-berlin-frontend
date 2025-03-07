2fd3f846c1a7df8546ae2ca060b312425989d4ea
beybora <bora.b.sarikaya@gmail.com>
2025-03-07 19:44:08 +0100

    refactor: move district sorting & grouping to utils

--- utils/getDistrictRepresentatives.js
+++ utils/getDistrictRepresentatives.js
@@ -1,35 +1,51 @@
 export const getDistrictRepresentatives = async () => {
-  const params = {
-    query: `
-        query DistrictRepresentativesQuery {
-            allDistrictRepresentatives {
-             nodes {
-                 acfFields {
-                     name
-                     district
-                     description
-                 }
-             }
+    const params = {
+      query: `
+          query DistrictRepresentativesQuery {
+              allDistrictRepresentatives {
+               nodes {
+                   acfFields {
+                       name
+                       district
+                       description
+                   }
+               }
+          }
         }
-      }
-      `,
+        `,
+    };
+  
+    const response = await fetch(process.env.WP_GRAPHQL_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    });
+  
+    const { data } = await response.json();
+  
+    console.log(data);
+  
+    if (!data?.allDistrictRepresentatives?.nodes) {
+      return null;
+    }
+  
+    const districtMap = new Map();
+  
+    data.allDistrictRepresentatives.nodes.forEach(rep => {
+      const bezirk = rep.acfFields.district;
+      if (!districtMap.has(bezirk)) districtMap.set(bezirk, []);
+      districtMap.get(bezirk).push(rep);
+    });
+  
+    const sortedDistricts = Array.from(districtMap.keys()).sort();
+  
+    const groupedAndSorted = sortedDistricts.map(district => ({
+      district,
+      representatives: districtMap.get(district),
+    }));
+  
+    return groupedAndSorted;
   };
-
-  const response = await fetch(process.env.WP_GRAPHQL_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(params),
-  });
-
-  const { data } = await response.json();
-
-  console.log(data);
-
-  if (!data?.allDistrictRepresentatives?.nodes) {
-    return null;
-  }
-
-  return data.allDistrictRepresentatives.nodes;
-};
+  
